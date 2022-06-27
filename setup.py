c340d1856ce4ccb7e20c2cbe7931fafeeb823784
Kalyan Dutia <kalyan.dutia@gmail.com>
2022-06-27 14:27:53 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Retrieving targets from climate laws and policies",
+    author="Climate Policy Radar <tech@climatepolicyradar.org>",
+)
